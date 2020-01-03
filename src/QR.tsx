d0e9ea8b2ee6014fa8034a02ace4efa0a6389e78
Jace <jacehensley@gmail.com>
2020-01-03 09:51:00 -0700

    Make QR accept canvas props

--- src/QR.tsx
+++ src/QR.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Config} from '@bloomprotocol/qr'
+import {FC, forwardProps} from 'react-forward-props'
 
 import {useDrawQRCode} from './useRenderQRCode'
 
 type QRProps = Config<any>
 
-export const QR: React.FC<QRProps> = props => {
+export const QR: FC<'canvas', QRProps> = props => {
   const canvasRef = useDrawQRCode(props)
 
-  return <canvas ref={canvasRef} />
+  return <canvas {...forwardProps(props, 'data', 'options')} ref={canvasRef} />
 }
